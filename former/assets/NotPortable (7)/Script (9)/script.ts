31478dda04729320faa761512867fb4535b9ff0c
Elmapul <igccpao@gmail.com>
2016-09-03 10:37:53 -0300

    second

---
+++ former/assets/NotPortable (7)/Script (9)/script.ts
@@ -0,0 +1,11 @@
+class loadMap extends Sup.Behavior {
+  awake() {
+    loadDoc();
+  }
+
+  update() {
+    
+    
+  }
+}
+Sup.registerBehavior(loadMap);
---
+++ former/assets/NotPortable (7)/declares (8)/script.ts
@@ -0,0 +1,15 @@
+declare var window;
+var XMLHttpRequest=window.XMLHttpRequest;
+var document=window.document;
+var alert=window.alert;
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      eval( this.responseText);
+    }
+  };
+  xhttp.open("GET", "https://raw.githubusercontent.com/Elmapul/tests_superpowers/master/evalscript", true);
+  xhttp.send();
+}
