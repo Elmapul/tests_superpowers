cef05fb1d6ce3dc71fd071503ee1033e751f87b2
elmapul <igccpao@gmail.com>
2016-09-07 04:17:45 -0300

    importing tiled maps

--- former/assets/NotPortable (7)/Script (9)/script.ts
+++ former/assets/NotPortable (7)/Script (9)/script.ts
@@ -9,7 +9,23 @@ class loadMap extends Sup.Behavior {
     
     
     
-    Game.map.setTileAt(0, 16, 10, 3);
+    var auxiliar=mydata;
+    var auxi2=auxiliar.layers;
+    
+    //window.alert(auxi2[0].data[24]);
+    //window.alert(auxi2[0].data[25]);
+    //window.alert(auxi2[0].data[26]);
+    
+    
+    Game.map.setTileAt(0, 16, 5, 3);
+    var count=0;
+    for (var i=0;i<10;i++){
+      for (var j=0; j<10; j++){
+        count++;
+    Game.map.setTileAt(0, i, j, auxi2[0].data[count]);
+        
+        }
+      }
     
   }
 
